4d48a4e6935afe6c1f9dc87e3018620d7b58581a
RippandT <56902025+RippandT@users.noreply.github.com>
2019-11-19 11:40:50 +0800

    Implemented the typing effect

--- typing.js
+++ typing.js
@@ -1,12 +1,11 @@
-var txt = 'PROGRAM THE FUTURE. TODAY.'; /*picture text*/ 
-var speed = 50; /*speed in which the text it typed out.*/ 
-var pos=0;
-type();
+var i = 0;
+var txt = 'PROGRAM THE FUTURE. TODAY.'; /* The text */
+typeWriter();
 
-function type() { 
-    if (pos<str.length) {
-        document.getElementById("program").innerHTML += txt.charAt(pos);
-        pos++;
-        setTimeOut(type, speed); //call
+function typeWriter() {
+if (i < txt.length) {
+    document.getElementById("programming").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 50);
     }
 }
