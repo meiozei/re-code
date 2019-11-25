53a17128a68c56cdf98ad02194c99f94f606eebd
Meiozei <53755879+yohamaruki@users.noreply.github.com>
2019-11-26 07:36:13 +0800

    FINAL

---
+++ typing.js
@@ -0,0 +1,12 @@
+  
+var i = 0;
+var txt = 'PROGRAM THE FUTURE. TODAY.'; /* The text */
+typeWriter();
+
+function typeWriter() {
+if (i < txt.length) {
+    document.getElementById("programming").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 50);
+    }
+}
